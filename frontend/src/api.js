3429eebff391cfb6b5afaac3eee58618a21565b5
Varad Wanwase <varad.wanwase@vinsys.com>
2026-04-03 15:08:00 +0530

    s added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://job-tracker-backend-hvt8.onrender.com/api"
+    baseURL: "https://job-tracker-backend-hvt8.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
