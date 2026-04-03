7618f859f2650cb400c12e1fc5bdd7958e0fde26
Varad Wanwase <varad.wanwase@vinsys.com>
2026-04-03 14:16:05 +0530

    Login page moved

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import API from './api';
+import API from '../api';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
